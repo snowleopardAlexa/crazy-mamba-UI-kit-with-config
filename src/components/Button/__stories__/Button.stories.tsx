69fef961b677ced4a933c9e5c63ab11d45cafa7a
snowleopardAlexa <alexaslomski@gmail.com>
2021-09-27 21:40:10 -0400

    add button code

--- src/components/Button/__stories__/Button.stories.tsx
+++ src/components/Button/__stories__/Button.stories.tsx
@@ -1,50 +1,16 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
-const css = require('./buttonStory.scss');
+
+import "../button.scss";
 import Button from "../";
 
-const stories = storiesOf("Components/Button", module);
+const stories = storiesOf("Button", module);
 stories.addDecorator(withKnobs);
 
-stories.add("Default", () => {
+stories.add("Button", () => {
     return (
-        <div className={css.container}>
-            <h1>Button</h1>
-            <p>Some information about the button</p>
-
-            <h2>Button variation 1</h2>
-            <Button 
-            color={'primary'} 
-            text={'button 1'} 
-            label={'button 1'} 
-            iconName={'chrome'}
-            hasIcon
-            onClick={() => alert('hello world')}
-            loading
-            />
-
-            <h2>Button variation 2</h2>
-            <Button 
-            color={'primary'} 
-            text={'button 2'} 
-            label={'button 2'} 
-            inverted iconName={'IE'}
-            iconColor={'body'}
-            hasIcon
-            />
-
-            <h2>Button variation 3</h2>
-            <Button 
-            color={'secondary'} 
-            text={'button 3'} 
-            label={'button 3'} 
-            disabled={true} 
-            iconName={'opera'}
-            hasIcon={false}
-            />
-
-
-        </div>
+        <Button />
     );
 });
+
